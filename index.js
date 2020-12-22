87b903c4978f1f56159a2d99d6adfb283b39d63b
Darcie <64379406+dspringall@users.noreply.github.com>
2020-12-22 19:55:21 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// const book = document.querySelector('.submit');
+// const guest = document.querySelector('#name').value;
+// const date = document.querySelector('#start').value;
+// const phone = document.querySelector('#phone').value;
+// const group = document.querySelector('#group').value;
+
+// // book.disabled = true;
+
+// book.addEventListener('click', function () {
+//     const guest = document.querySelector('#name').value;
+//     alert(`Thank you for your booking, ${guest}.`)
+// });
